10eeb6a72de8b06154fc443967314150c926318b
TimChen <chih-ting.chen@cgu.edu>
2020-03-31 22:46:55 -0700

    Create add Circle&Arrow function"

--- home.js
+++ home.js
@@ -4,14 +4,14 @@ var canvas = new fabric.Canvas("c");
 // create a rectangle object
 var rect = new fabric.Rect({
   left: 100,
-  top: 100,
+  top: 200,
   fill: "red",
   width: 70,
   height: 70
 });
 
 // "add" rectangle onto canvas
-canvas.add(rect).setActiveObject(rect);
+canvas.add(rect);
 
 //draw a circle
 var circle = new fabric.Circle({
@@ -40,7 +40,20 @@ $("#b").click(function() {
   canvas.renderAll();
 });
 
+function addCircle() {
+  var circle = new fabric.Circle({
+    radius: 55,
+    fill: "green",
+    left: 205,
+    top: 255
+  });
 
+  canvas.add(circle);
+}
+
+function addArrow() {
+  drawArrow(100, 100, 180, 100);
+}
 
 // change rectangle color
 function changeRColor() {
@@ -67,3 +80,84 @@ function changeTColor() {
   triangle.set("fill", triVal);
   canvas.renderAll();
 }
+
+function drawArrow(fromx, fromy, tox, toy) {
+  var angle = Math.atan2(toy - fromy, tox - fromx);
+
+  var headlen = 15; // arrow head size
+
+  // bring the line end back some to account for arrow head.
+  tox = tox - headlen * Math.cos(angle);
+  toy = toy - headlen * Math.sin(angle);
+
+  // calculate the points.
+  var points = [
+    {
+      x: fromx, // start point
+      y: fromy
+    },
+    {
+      x: fromx - (headlen / 4) * Math.cos(angle - Math.PI / 2),
+      y: fromy - (headlen / 4) * Math.sin(angle - Math.PI / 2)
+    },
+    {
+      x: tox - (headlen / 4) * Math.cos(angle - Math.PI / 2),
+      y: toy - (headlen / 4) * Math.sin(angle - Math.PI / 2)
+    },
+    {
+      x: tox - headlen * Math.cos(angle - Math.PI / 2),
+      y: toy - headlen * Math.sin(angle - Math.PI / 2)
+    },
+    {
+      x: tox + headlen * Math.cos(angle), // tip
+      y: toy + headlen * Math.sin(angle)
+    },
+    {
+      x: tox - headlen * Math.cos(angle + Math.PI / 2),
+      y: toy - headlen * Math.sin(angle + Math.PI / 2)
+    },
+    {
+      x: tox - (headlen / 4) * Math.cos(angle + Math.PI / 2),
+      y: toy - (headlen / 4) * Math.sin(angle + Math.PI / 2)
+    },
+    {
+      x: fromx - (headlen / 4) * Math.cos(angle + Math.PI / 2),
+      y: fromy - (headlen / 4) * Math.sin(angle + Math.PI / 2)
+    },
+    {
+      x: fromx,
+      y: fromy
+    }
+  ];
+
+  var pline = new fabric.Polyline(points, {
+    fill: "black",
+    stroke: "black",
+    opacity: 1,
+    strokeWidth: 2,
+    originX: "left",
+    originY: "top",
+    selectable: true
+  });
+
+  canvas.add(pline);
+
+  canvas.renderAll();
+}
+
+//draw an arrow!
+
+// // click and drag to draw more arrow!
+// var startX, startY, endX, endY;
+
+// canvas.on('mouse:down', function() {
+//     var pointer = canvas.getPointer(event.e);
+//     startX = pointer.x;
+//     startY = pointer.y;
+// });
+// canvas.on('mouse:up', function() {
+//    	var pointer = canvas.getPointer(event.e);
+//     endX = pointer.x;
+//     endY = pointer.y;
+//     drawArrow(startX, startY, endX, endY);
+// });
